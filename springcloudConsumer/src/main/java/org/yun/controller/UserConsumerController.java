2f2ce99a46c6f17a431571fb01189396241d8ec6
yc <279611480@qq.com>
2019-10-31 16:06:28 +0800

    ...

--- springcloudConsumer/src/main/java/org/yun/controller/UserConsumerController.java
+++ springcloudConsumer/src/main/java/org/yun/controller/UserConsumerController.java
@@ -19,13 +19,13 @@ public class UserConsumerController {
 
     @Autowired
     RestTemplate restTemplate;
-
+    //http://localhost:8003/consumer/add
     @RequestMapping("/add")
     public void addUser(){
         //get请求
         restTemplate.getForEntity(REST_URL_PREFIX + "/user" + "/addOrEditUser",void.class);
     }
-
+    //http://localhost:8003/consumer/selectById/1
     @RequestMapping(value="/selectById/{id}")
     public UserVO get(@PathVariable("id") String id){
         //查看拼URL  拼成什么样了
@@ -33,7 +33,7 @@ public class UserConsumerController {
         UserVO userVO = restTemplate.getForObject(REST_URL_PREFIX + "/user" + "/selectById" + "/" + id, UserVO.class);
         return userVO;
     }
-
+    //http://localhost:8003/consumer/selectAll
     @RequestMapping(value="/selectAll")
     public List<UserVO> selectAll(){
         List list = restTemplate.getForObject(REST_URL_PREFIX + "/user/selectAll", List.class);
